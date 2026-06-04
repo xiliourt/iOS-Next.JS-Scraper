d2eda6a49222a5c4d732a1f6754ce70af4bd19f4
xiliourt <admin@xiliourt.ovh>
2026-06-04 13:05:29 +1000

    Update scraper.ts

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -8,7 +8,7 @@ export const getProducts = async (countryCode: string, appId: string) => {
     try {
         while (nextUrl) {
             // Ensure full URL is formed if the next link is relative
-            const targetUrl: string = nextUrl.startsWith('http') ? nextUrl : `https://apps.apple.com/api/apps/${nextUrl}`;
+            const targetUrl: string = nextUrl.startsWith('http') ? nextUrl : `https://apps.apple.com/api/apps/${nextUrl}?platform=iphone`;
 
             const response = await fetch(targetUrl, {
                 headers: {
